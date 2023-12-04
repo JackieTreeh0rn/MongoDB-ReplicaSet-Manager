dd030d516b1d4beb2be87a4866697af881f4a45e
fsck <fsck66@users.noreply.github.com>
2023-12-03 21:17:36 -0500

    -updated deploy script for new docker-compose

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 set -o allexport
 . ./mongo-rs.env
 
-# Use to avoid subnet overlaps - specifies subnet
+# Use to avoid subnet overlaps - specify your own subnet
 # docker network create --opt encrypted -d overlay --subnet=10.0.25.0/24 --gateway=10.0.25.1  ${BACKEND_NETWORK_NAME}
 
 docker network create --opt encrypted -d overlay ${BACKEND_NETWORK_NAME}
 openssl rand -base64 741 | docker secret create mongo-keyfile -
-docker stack deploy -c docker-compose.yml ${STACK_NAME}
+docker stack deploy -c docker-compose-stack.yml ${STACK_NAME}
